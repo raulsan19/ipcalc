42e8bba514540a34e8c817de244bbb1ec366b0ba
raulsan19 <raulsantoslopez11@gmail.com>
2024-01-16 17:45:24 +0000

    Versió 1.6 del projecte ipcalc

--- app/index.php
+++ app/index.php
@@ -9,6 +9,6 @@
 	<body>
 		<p><b>CALCUALDORA IPv4</b></p>
 		<a href="ip.html">Aplicació de càlcul de subxarxes</a>
-                <p>Autor: santos</p>
+                <p>Autor: pantos</p>
 	</body>
 </html>
